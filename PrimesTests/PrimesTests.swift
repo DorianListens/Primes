d929695e658e7ab4ddc089d796b0859114539b60
Dorian <dorian.scheidt@gmail.com>
2015-07-20 12:37:01 -0400

    Initial Commit

---
+++ PrimesTests/PrimesTests.swift
@@ -0,0 +1,36 @@
+//
+//  PrimesTests.swift
+//  PrimesTests
+//
+//  Created by Dorian Scheidt on 2015-07-20.
+//  Copyright (c) 2015 Dorian Listens. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PrimesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
